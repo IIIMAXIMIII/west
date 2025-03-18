242cb9a25017aea4e220502ff3588d1cb32d08c2
vladr <vladreva57@gmail.com>
2025-03-18 17:53:01 +0500

    Duck and Dog

--- src/index.js
+++ src/index.js
@@ -27,25 +27,30 @@ function getCreatureDescription(card) {
     return 'Существо';
 }
 
+class Duck extends Card {
+    constructor(name = "Мирная утка", maxPower = 2) {
+        super(name, maxPower);
+    }
 
+    quacks() {
+        console.log('Кря!');
+    }
 
-// Основа для утки.
-function Duck() {
-    this.quacks = function () { console.log('quack') };
-    this.swims = function () { console.log('float: both;') };
+    swims() {
+        console.log('Плывет!');
+    }
 }
 
-
-// Основа для собаки.
-function Dog() {
+class Dog extends Card {
+    constructor(name = "Пес-бандит", maxPower = 3) {
+        super(name, maxPower);
+    }
 }
-
 
 // Колода Шерифа, нижнего игрока.
 const seriffStartDeck = [
     new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
+    new Dog()
 ];
 
 // Колода Бандита, верхнего игрока.
